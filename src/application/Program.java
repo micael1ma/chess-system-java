bb34949ecb4c0dae02e7077613ada5ea3de235df
micael1ma <micael1ma.professional@gmail.com>
2024-09-29 20:32:19 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello word");
+		Position newP = new Position(3, 3);
+		System.out.println(newP);
 
 	}
 
